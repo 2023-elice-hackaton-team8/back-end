dae12b420ad90ab9be472f4f752411ab6b40d0b8
Bu Sun Kim <busunkim96@gmail.com>
2023-08-26 23:52:13 +0900

    change python OCR to accept json payload

--- python-server/main.py
+++ python-server/main.py
@@ -7,16 +7,22 @@ import os
 app = Flask(__name__)
 vision_client = OCR()
 
+
 @app.route("/")
 def index():
     return "Index Page"
 
+
 @app.route("/ocr", methods=["POST"])
 def ocr():
     # Example: https://ojy1-bucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20230826_230308602.jpg
-    uri = request.form['uri']
-    return vision_client.get_text_from_uri(uri)
+    content_type = request.headers.get("Content-Type")
 
+    if content_type == "application/json":
+        uri = request.json["uri"]
+        return vision_client.get_text_from_uri(uri)
+    else:
+        return "Content type is not supported."
 
 
 if __name__ == "__main__":
